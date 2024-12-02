6f4a02b7d4ccaffbd639535016f074fdaae29238
szhdani <71515092+szhdani@users.noreply.github.com>
2024-12-02 13:16:24 +0100

    CORS, user model properties
    Added CORS, and changed user model properties order

--- backend/api/app/app.ts
+++ backend/api/app/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import loginRouter from "../login/router";
 import registrationRouter from "../registration/router";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use("/api/login", loginRouter);
 app.use("/api/registration", registrationRouter);
 
--- backend/api/models/user.ts
+++ backend/api/models/user.ts
@@ -1,7 +1,7 @@
 export class User{
     ID?: number;
     username?: string;
-    password?: string;
     email?: string;
+    password?: string;
     roleID?: number;
 }
