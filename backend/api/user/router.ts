2c3d9f8d9e5edfaff37580524535279419ac7cca
szhdani <71515092+szhdani@users.noreply.github.com>
2025-01-09 20:08:17 +0100

    User self update and delete
    Added endpoint for user self update and delete

--- backend/api/user/router.ts
+++ backend/api/user/router.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { GetUsers, GetUserById, UpdateUser, UpdateUserRole, DeleteUser } from "./user";
+import { GetUsers, GetUserById, UpdateUserSelf, UpdateUser, UpdateUserRole, DeleteUserSelf, DeleteUser } from "./user";
 import AuthService from "../services/auth";
 
 const router: Router = Router();
 
 router.get("/", AuthService.DecodeToken, AuthService.UserExists, AuthService.IsUserAdmin, GetUsers);
 router.get("/:ID", AuthService.DecodeToken, AuthService.UserExists, AuthService.IsUserItselfOrAdmin, GetUserById);
-router.put("/:ID", AuthService.DecodeToken, AuthService.UserExists, AuthService.IsUserItselfOrAdmin, UpdateUser);
+router.put("", AuthService.DecodeToken, AuthService.UserExists, UpdateUserSelf);
+router.put("/:ID", AuthService.DecodeToken, AuthService.UserExists, AuthService.IsUserAdmin, UpdateUser);
 router.put("/:ID/role", AuthService.DecodeToken, AuthService.UserExists, AuthService.IsUserAdmin, UpdateUserRole);
-router.delete("/:ID", AuthService.DecodeToken, AuthService.UserExists, AuthService.IsUserItselfOrAdmin, DeleteUser);
+router.delete("", AuthService.DecodeToken, AuthService.UserExists, DeleteUserSelf);
+router.delete("/:ID", AuthService.DecodeToken, AuthService.UserExists, AuthService.IsUserAdmin, DeleteUser);
 
 export default router;
--- backend/api/user/user.ts
+++ backend/api/user/user.ts
@@ -31,6 +31,53 @@ export async function GetUserById(req: any, res: Response){
     });
 }
 
+export async function UpdateUserSelf(req: any, res: Response){
+    const user = new User();
+    user.ID = req.decodedToken.userID;
+    user.username = req.body.username;
+
+    if(!user.username){
+        res.status(400).send({error: "Hiányzó adatok"});
+        return;
+    }
+
+    const currentUser = await UserService.GetUserById(user)
+    .catch((err) => {
+        console.log(err);
+        res.status(500).send({error: "Hiba az adatbázis kapcsolat során"});
+        return;
+    });
+
+    if(!currentUser){
+        res.status(400).send({error: "Nem létezik ilyen felhasználó"});
+        return;
+    }
+
+    if(currentUser.username == user.username){
+        res.status(400).send({error: "Az új felhasználónév nem egyezhet meg a régi felhasználónévvel"});
+        return;
+    }
+
+    await UserService.UpdateUser(user)
+    .then(async (result) => {
+        if(!result.affectedRows){
+            res.status(500).send({error: "Hiba a felhasználó frissítése során"});
+            return;
+        }
+        res.status(200).send({message: "Felhasználó sikeresen frissítve"});
+        return;
+    })
+    .catch((err) => {
+        console.log(err);
+        if(err.errno == 1062){
+            res.status(400).send({error: "Ez a felhasználónév már használatban van"});
+            return;
+        }
+        res.status(500).send({error: "Hiba az adatbázis kapcsolat során"});
+        return;
+    });
+}
+
 export async function UpdateUser(req: any, res: Response){
     const user = new User();
     user.ID = req.params.ID;
@@ -128,6 +175,38 @@ export async function UpdateUserRole(req: any, res: Response){
 export async function DeleteUser(req: any, res: Response){
     const user = new User();
     user.ID = req.params.ID;
+
+    const currentUser = await UserService.GetUserById(user)
+    .catch((err) => {
+        console.log(err);
+        res.status(500).send({error: "Hiba az adatbázis kapcsolat során"});
+        return;
+    });
+
+    if(!currentUser){
+        res.status(400).send({error: "Nem létezik ilyen felhasználó"});
+        return;
+    }
+
+    await UserService.DeleteUser(user)
+    .then(async (result) => {
+        if(!result.affectedRows){
+            res.status(500).send({error: "Hiba a felhasználó törlése során"});
+            return;
+        }
+        res.status(200).send({message: "Felhasználó sikeresen törölve"});
+        return;
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).send({error: "Hiba az adatbázis kapcsolat során"});
+        return;
+    });
+}
+
+export async function DeleteUserSelf(req: any, res: Response){
+    const user = new User();
+    user.ID = req.decodedToken.userID;
 
     const currentUser = await UserService.GetUserById(user)
     .catch((err) => {
