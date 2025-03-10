7c1c361ba177c9f52cfbdbdc0a0540f131424609
Szabó-Holman Dániel <szhdani@gmail.com>
2025-03-10 11:18:02 +0100

    Fixed error where recipe was not found when getting multiple recipes at a time.
    Fixed error where recipe was not found when getting multiple recipes at a time.

--- backend/api/recipe/router.ts
+++ backend/api/recipe/router.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import { GetRecipeByID, GetAcceptedRecipes, GetWaitingRecipes, GetDraftRecipes, GetRecipesByUser, NewRecipe, UpdateRecipeByID, AcceptRecipeByID, RejectRecipeByID, DeleteRecipeByID } from "./recipe";
+import { GetAcceptedRecipes, GetWaitingRecipes, GetDraftRecipes, GetRecipesByUser, GetRecipeByID, NewRecipe, UpdateRecipeByID, AcceptRecipeByID, RejectRecipeByID, DeleteRecipeByID } from "./recipe";
 import AuthService from "../services/auth";
 
 const router: Router = Router();
 
-router.get("/:ID", AuthService.DecodeToken, AuthService.UserExists, AuthService.RecipeExists, GetRecipeByID);
 router.get("/accepted", AuthService.DecodeToken, AuthService.UserExists, GetAcceptedRecipes);
 router.get("/waiting", AuthService.DecodeToken, AuthService.UserExists, AuthService.IsUserModeratorOrAdmin, GetWaitingRecipes);
 router.get("/draft", AuthService.DecodeToken, AuthService.UserExists, AuthService.IsUserAdmin, GetDraftRecipes);
 router.get("/mine", AuthService.DecodeToken, AuthService.UserExists, GetRecipesByUser);
+router.get("/:ID", AuthService.DecodeToken, AuthService.UserExists, AuthService.RecipeExists, GetRecipeByID);
 router.post("", AuthService.DecodeToken, AuthService.UserExists, NewRecipe);
 router.put("/:ID", AuthService.DecodeToken, AuthService.UserExists, AuthService.RecipeExists, AuthService.IsUserUploader, UpdateRecipeByID);
 router.put("/:ID/accept", AuthService.DecodeToken, AuthService.UserExists, AuthService.RecipeExists, AuthService.IsUserModeratorOrAdmin, AcceptRecipeByID);
